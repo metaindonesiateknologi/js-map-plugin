2cf86eaac9f970df3ad8bba4cfa8f8a8fc2f5224
Mohammad Soleh <ibnuzeinhamzah@gmail.com>
2025-01-25 20:53:38 +0700

    update

--- src/server/mitmap_node_plugin.d.ts
+++ src/server/mitmap_node_plugin.d.ts
@@ -1,8 +1,5 @@
 /* tslint:disable */
 /* eslint-disable */
 export function search_address_by_name(text: string, appid: string, token: string): Promise<any>;
-export function translate_byname_fn(body: string): string;
 export function search_address_by_coord(lat_str: string, lon_str: string, appid: string, token_str: string): Promise<any>;
-export function translate_bycoord_fn(body: string): string;
 export function map_route(start: string, start_lat: string, start_lon: string, dest: string, dest_lat: string, dest_lon: string, appid: string, token: string): Promise<any>;
-export function translate_route_fn(body: string): string;
--- src/server/mitmap_node_plugin.js
+++ src/server/mitmap_node_plugin.js
@@ -219,25 +219,6 @@ module.exports.search_address_by_name = function(text, appid, token) {
 };
 
 /**
- * @param {string} body
- * @returns {string}
- */
-module.exports.translate_byname_fn = function(body) {
-    let deferred2_0;
-    let deferred2_1;
-    try {
-        const ptr0 = passStringToWasm0(body, wasm.__wbindgen_malloc, wasm.__wbindgen_realloc);
-        const len0 = WASM_VECTOR_LEN;
-        const ret = wasm.translate_byname_fn(ptr0, len0);
-        deferred2_0 = ret[0];
-        deferred2_1 = ret[1];
-        return getStringFromWasm0(ret[0], ret[1]);
-    } finally {
-        wasm.__wbindgen_free(deferred2_0, deferred2_1, 1);
-    }
-};
-
-/**
  * @param {string} lat_str
  * @param {string} lon_str
  * @param {string} appid
@@ -255,25 +236,6 @@ module.exports.search_address_by_coord = function(lat_str, lon_str, appid, token
     const len3 = WASM_VECTOR_LEN;
     const ret = wasm.search_address_by_coord(ptr0, len0, ptr1, len1, ptr2, len2, ptr3, len3);
     return ret;
-};
-
-/**
- * @param {string} body
- * @returns {string}
- */
-module.exports.translate_bycoord_fn = function(body) {
-    let deferred2_0;
-    let deferred2_1;
-    try {
-        const ptr0 = passStringToWasm0(body, wasm.__wbindgen_malloc, wasm.__wbindgen_realloc);
-        const len0 = WASM_VECTOR_LEN;
-        const ret = wasm.translate_bycoord_fn(ptr0, len0);
-        deferred2_0 = ret[0];
-        deferred2_1 = ret[1];
-        return getStringFromWasm0(ret[0], ret[1]);
-    } finally {
-        wasm.__wbindgen_free(deferred2_0, deferred2_1, 1);
-    }
 };
 
 /**
@@ -308,31 +270,12 @@ module.exports.map_route = function(start, start_lat, start_lon, dest, dest_lat,
     return ret;
 };
 
-/**
- * @param {string} body
- * @returns {string}
- */
-module.exports.translate_route_fn = function(body) {
-    let deferred2_0;
-    let deferred2_1;
-    try {
-        const ptr0 = passStringToWasm0(body, wasm.__wbindgen_malloc, wasm.__wbindgen_realloc);
-        const len0 = WASM_VECTOR_LEN;
-        const ret = wasm.translate_route_fn(ptr0, len0);
-        deferred2_0 = ret[0];
-        deferred2_1 = ret[1];
-        return getStringFromWasm0(ret[0], ret[1]);
-    } finally {
-        wasm.__wbindgen_free(deferred2_0, deferred2_1, 1);
-    }
-};
-
 function __wbg_adapter_26(arg0, arg1, arg2) {
-    wasm.closure101_externref_shim(arg0, arg1, arg2);
+    wasm.closure102_externref_shim(arg0, arg1, arg2);
 }
 
-function __wbg_adapter_93(arg0, arg1, arg2, arg3) {
-    wasm.closure140_externref_shim(arg0, arg1, arg2, arg3);
+function __wbg_adapter_92(arg0, arg1, arg2, arg3) {
+    wasm.closure141_externref_shim(arg0, arg1, arg2, arg3);
 }
 
 const __wbindgen_enum_RequestCredentials = ["omit", "same-origin", "include"];
@@ -408,6 +351,10 @@ module.exports.__wbg_iterator_9a24c88df860dc65 = function() {
     return ret;
 };
 
+module.exports.__wbg_log_c222819a41e063d3 = function(arg0) {
+    console.log(arg0);
+};
+
 module.exports.__wbg_new_018dcc2d6c8c2f6a = function() { return handleError(function () {
     const ret = new Headers();
     return ret;
@@ -420,7 +367,7 @@ module.exports.__wbg_new_23a2665fac83c611 = function(arg0, arg1) {
             const a = state0.a;
             state0.a = 0;
             try {
-                return __wbg_adapter_93(a, state0.b, arg0, arg1);
+                return __wbg_adapter_92(a, state0.b, arg0, arg1);
             } finally {
                 state0.a = a;
             }
@@ -583,8 +530,8 @@ module.exports.__wbindgen_cb_drop = function(arg0) {
     return ret;
 };
 
-module.exports.__wbindgen_closure_wrapper294 = function(arg0, arg1, arg2) {
-    const ret = makeMutClosure(arg0, arg1, 102, __wbg_adapter_26);
+module.exports.__wbindgen_closure_wrapper292 = function(arg0, arg1, arg2) {
+    const ret = makeMutClosure(arg0, arg1, 103, __wbg_adapter_26);
     return ret;
 };
 
